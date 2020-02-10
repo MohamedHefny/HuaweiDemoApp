c50e89a69fecb474a5679e358460aec11fb25545
Mohamed Hefny <hefny.7@gmail.com>
2020-02-10 02:15:38 +0200

    Change video URL. Add product price in design.

--- app/src/main/java/com/mohamedhefny/huawei/utils/Constants.kt
+++ app/src/main/java/com/mohamedhefny/huawei/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.mohamedhefny.huawei.utils
 
 const val REQ_CODE_BUY = 102
 
-const val VIDEO_URL = "https://firebasestorage.googleapis.com/v0/b/efrshly.appspot.com/o/TheHuaweiGiftFactory.mp4?alt=media&token=4c608767-012e-4c67-8ec0-432506b690d8"
+const val VIDEO_URL = "https://download1593.mediafire.com/oxj7u0inpvgg/yho6rz5mg2ulhm3/TheHuaweiGiftFactory.mp4"
 
 const val PUBLIC_KEY =
     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgD+hXlrcEEKGitERy5Ighcl/G9LZfaWxeyCRJ6/T0YZ7pgXGAExseOyNejq/g3rRdL+Y+LEjkLTGXhn2+m7Nf/nx0+rlFxbNgTpyCkk4GZTM+gRibI/hSpn4h2jBDxu7DNQNuUL1f4TVNSjWA6L7BQ5qHL/OdxMTGqv/Nj0nrvrQFUNBpAu2wygLpwjb4bwMa5wbZ8KyA2gCPqVUJyOBJ1NhS+MpySPCo5MAVViVA75zK7b36nwzCMJ4lHiv0uHQj+9VflK8Q7VmrCKqzc3RVEu6R+KevQQhcE03gtZsCxTpP+HAEdzcf7YYeBsuDHo4fU3z7zQCcGPCHByWXhDc1wIDAQAB"
